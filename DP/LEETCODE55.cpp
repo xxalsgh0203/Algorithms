53ed202b02795b303e99ded708f37011e9349c48
Minho Kim <music4537@naver.com>
2022-01-17 15:38:41 -0600

    leetcode55

---
+++ DP/LEETCODE55.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reachable = 0;
+        int n = nums.size();
+
+        for(int i=0; i<n; i++){
+            if(i > reachable)
+                return false;
+            
+            reachable = max(reachable, i+nums[i]);
+        }
+
+        return true;
+    }
+};
