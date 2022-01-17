21cb450d498ac8ca67194ff4ac6c7808cdf0591e
Minho Kim <music4537@naver.com>
2022-01-17 16:04:55 -0600

    leetcode55

--- DP/LEETCODE55.cpp
+++ DP/LEETCODE55.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
 using namespace std;
 
-class Solution {
-public:
-    bool canJump(vector<int>& nums) {
-        int reachable = 0;
-        int n = nums.size();
+// 1. Linear solution
+// class Solution {
+// public:
+//     bool canJump(vector<int>& nums) {
+//         int reachable = 0;
+//         int n = nums.size();
 
-        for(int i=0; i<n; i++){
-            if(i > reachable)
-                return false;
+//         for(int i=0; i<n; i++){
+//             if(i > reachable)
+//                 return false;
             
-            reachable = max(reachable, i+nums[i]);
+//             reachable = max(reachable, i+nums[i]);
+//         }
+
+//         return true;
+//     }
+// };
+
+class Solution {
+public: 
+    bool canJump(vector<int>& nums){
+        int n = nums.size();
+        vector<int> dp(n, 0);
+
+        dp[0] = true;
+
+        for(int i=1; i<n; i++){
+            for(int j=i-1; j>=0; j--){
+                if(dp[j] && j+nums[j]>=i){
+                    dp[i] = true;
+                    break;
+                }
+            }
         }
 
-        return true;
+        return dp[n-1];
     }
 };
