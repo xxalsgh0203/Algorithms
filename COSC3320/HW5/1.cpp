b6f8c7d463bfcdb3008ff90bee0f7b459bbb29cd
Minho Kim <music4537@naver.com>
2022-12-07 14:34:47 -0600

    p1

--- COSC3320/HW5/1.cpp
+++ COSC3320/HW5/1.cpp
@@ -1,23 +1,60 @@
+// C++ Code for Palindrome Partitioning
+// Problem
 #include <iostream>
+#include <string.h>
+#include <limits.h>
 #include <vector>
 
 using namespace std;
+ 
+string str;
 
-int main(){
+int findSmallestPal(){
+    int n = str.length();
+    vector<vector<int>> Pal(n, vector<int> (n, 0));
 
-    vector<int> d1(n),  d2(n);
-    for (int i = 0; i < n; i++) {
-        d1[i] = 1;
+    for(int i=0; i<n; i++){
+        Pal[i][i] = 1;
+    }
 
-        while (0 <= i - d1[i] && i + d1[i] < n && s[i - d1[i]] == s[i + d1[i]]) {
-            d1[i]++;
-        }
-
-        d2[i] = 0;
-
-        while (0 <= i - d2[i] - 1 && i + d2[i] < n && s[i - d2[i] - 1] == s[i + d2[i]]) {
-            d2[i]++;
+    for(int i=0; i<n-1; i++){
+        if(str[i] == str[i+1]){
+            Pal[i][i+1] = 1;
         }
     }
 
+    for(int length=3; length<n+1; length++){
+        for(int i=0; i<n-length+1; i++){
+            int j = i + length - 1;
+            if(str[i] == str[j] && Pal[i+1][j-1])
+                Pal[i][j] = 1;
+        }
+    }
+
+    vector<int> Splits(n, 0);
+
+    for(int i=0; i<n; i++){
+        int smallest = str.length() - 1;
+        if(Pal[0][i]){
+            Splits[i] = 0;
+        }
+        else{
+            for(int j=0; j<i; j++){
+                if(Pal[j+1][i] && Splits[j] + 1 < smallest){
+                    smallest = Splits[j] + 1;
+                }
+            }
+            Splits[i] = smallest;
+        }
+    }
+
+    return Splits[n-1] + 1;
+}
+
+int main(){
+    int size;
+    cin >> size;
+    cin >> str;
+
+    cout << findSmallestPal();
 }
