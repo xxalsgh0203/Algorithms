33b2bb4eeee91e36e2cad5f1801e42e42a6fff9c
Minho Kim <music4537@naver.com>
2022-12-07 20:26:19 -0600

    p2

--- COSC3320/HW5/1.cpp
+++ COSC3320/HW5/1.cpp
@@ -13,17 +13,17 @@ int findSmallestPal(){
     int n = str.length();
     vector<vector<int>> Pal(n, vector<int> (n, 0));
 
-    for(int i=0; i<n; i++){
+    for(int i = 0; i < n; i++){
         Pal[i][i] = 1;
     }
 
-    for(int i=0; i<n-1; i++){
+    for(int i = 0; i < n-1; i++){
         if(str[i] == str[i+1]){
             Pal[i][i+1] = 1;
         }
     }
 
-    for(int length=3; length<n+1; length++){
+    for(int length = 3; length < n+1; length++){
         for(int i=0; i<n-length+1; i++){
             int j = i + length - 1;
             if(str[i] == str[j] && Pal[i+1][j-1])
@@ -33,18 +33,18 @@ int findSmallestPal(){
 
     vector<int> Splits(n, 0);
 
-    for(int i=0; i<n; i++){
-        int smallest = str.length() - 1;
+    for(int i = 0; i < n; i++){
+        int minnum = str.length() - 1;
         if(Pal[0][i]){
             Splits[i] = 0;
         }
         else{
-            for(int j=0; j<i; j++){
-                if(Pal[j+1][i] && Splits[j] + 1 < smallest){
-                    smallest = Splits[j] + 1;
+            for(int j = 0; j < i; j++){
+                if(Pal[j+1][i] && Splits[j] + 1 < minnum){
+                    minnum = Splits[j] + 1;
                 }
             }
-            Splits[i] = smallest;
+            Splits[i] = minnum;
         }
     }
 
---
+++ COSC3320/HW5/2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int numofNodes, numofEdges;
+
+vector<int> Solve(int start, vector<pair<int,int> > graph[]){
+    vector <int> dist(numofNodes, 0);
+    priority_queue <pair<int, int> >  pq;
+    
+    pq.push(make_pair(0, start));
+
+    while(!pq.empty()){
+        int curNode = pq.top().second;
+        int curDist = pq.top().first;
+        pq.pop();
+
+        for(int i=0; i<graph[curNode].size(); i++){
+            int nextNode = graph[curNode][i].first;
+            int totalDist = graph[curNode][i].second + curDist;
+            if(dist[nextNode] < totalDist){
+                dist[nextNode] = totalDist;
+                pq.push(make_pair(totalDist, nextNode));
+            }
+        }
+    }
+
+    return dist;
+}
+
+int main(){
+    
+    int start, dest;
+
+    cin >> numofNodes >> numofEdges;
+    cin >> start >> dest;
+
+    vector<pair<int,int> > graph[numofNodes];
+
+    for(int i=0; i<numofEdges; i++){    
+        int startNode, destNode, distance;
+
+        cin >> startNode >> destNode >> distance;
+        graph[startNode].push_back(make_pair(destNode, distance));
+    }
+
+    vector<int> result;
+
+    result = Solve(start, graph);
+
+    // for(int i=0; i<result.size(); i++){
+    //     cout << result[i] << " ";
+    // }
+
+    cout << result[dest];
+
+    return 0;
+}
