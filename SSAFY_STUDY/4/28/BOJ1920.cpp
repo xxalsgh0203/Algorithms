91c7d2387d7470dde645f6ba2284d4076735ba8f
Minho Kim <music4537@naver.com>
2023-04-28 20:19:18 +0900

    ssafy algorithm study day 2

---
+++ SSAFY_STUDY/4/28/BOJ1920.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int Binary_Search(int number, vector<int> arr) {
+    int left = 0;
+    int right = arr.size() - 1;
+    while (left <= right) {
+        int middle = (left + right) / 2;
+        if (arr[middle] == number)
+            return 1;
+        if (arr[middle] > number) {
+            right = middle - 1;
+        } else if (arr[middle] < number) {
+            left = middle + 1;
+        }
+    }
+
+    return 0;
+}
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> arr;
+    for (int i = 0; i < N; i++) {
+        int A;
+        cin >> A;
+        arr.push_back(A);
+    }
+    sort(arr.begin(), arr.end());
+    int cnt;
+    cin >> cnt;
+    for (int i = 0; i < cnt; i++) {
+        int number;
+        cin >> number;
+        cout << Binary_Search(number, arr) << '\n';
+    }
+}
